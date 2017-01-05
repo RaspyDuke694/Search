c127d8de7de9e1ca666c9e80475b56fc8e6d5fb5
RaspyDuke694 <vishalvsy1996@outlook.com>
2017-01-05 15:09:28 +0530

    Winter Training Project

---
+++ src/search/Search.java
@@ -0,0 +1,21 @@
+
+package search;
+
+import java.awt.Frame;
+
+/**
+ *
+ * @author Vishal
+ */
+public class Search {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Frame w = new MainGUI();
+        w.pack();
+        w.setVisible(true);
+    }
+    
+}
